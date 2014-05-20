6dde3af8ea901f260ca0815be60532656b032767
TDiva <tanushadomanova@gmail.com>
2014-05-20 13:44:31 +0300

    Added problem package

---
+++ gen-open-shop/src/main/java/problem/Job.java
@@ -0,0 +1,26 @@
+package problem;
+
+import java.util.List;
+
+public class Job {
+	
+	List<Integer> operations;
+	
+
+	
+	public Job(List<Integer> operations) {
+		super();
+		this.operations = operations;
+	}
+
+	public List<Integer> getOperations() {
+		return operations;
+	}
+
+	public void setOperations(List<Integer> operations) {
+		this.operations = operations;
+	}
+	
+	
+
+}
