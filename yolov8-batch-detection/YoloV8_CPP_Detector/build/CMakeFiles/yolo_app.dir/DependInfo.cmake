e224da8e2327cb53500d0a7b08a0f19d26c23161
Kazi Abdul Jamil <jrobo786@outlook.com>
2025-07-01 14:58:50 +0200

    Added YOLOv8 projects to computer-vision-projects

---
+++ yolov8-batch-detection/YoloV8_CPP_Detector/build/CMakeFiles/yolo_app.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/jamil/projects/YoloV8_CPP_Detector/main.cpp" "CMakeFiles/yolo_app.dir/main.cpp.o" "gcc" "CMakeFiles/yolo_app.dir/main.cpp.o.d"
+  "/Users/jamil/projects/YoloV8_CPP_Detector/src/yolo_detector.cpp" "CMakeFiles/yolo_app.dir/src/yolo_detector.cpp.o" "gcc" "CMakeFiles/yolo_app.dir/src/yolo_detector.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
